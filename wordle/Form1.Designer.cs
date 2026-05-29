8f2f3772881ff1ec4c6c1a4a96fad0b31d85e3f0
warlockina <sofia.anghelescu.ro@gmail.com>
2026-05-29 19:19:42 +0300

    Changed generating labels to predefined ones

--- wordle/Form1.Designer.cs
+++ wordle/Form1.Designer.cs
@@ -29,36 +29,441 @@
         private void InitializeComponent()
         {
             lblTitle = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label15 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label20 = new Label();
+            label19 = new Label();
+            label18 = new Label();
+            label17 = new Label();
+            label16 = new Label();
+            label25 = new Label();
+            label24 = new Label();
+            label23 = new Label();
+            label22 = new Label();
+            label21 = new Label();
+            keyLabelQ = new Label();
+            keyLabelW = new Label();
             SuspendLayout();
             // 
             // lblTitle
             // 
-            lblTitle.AutoSize = true;
             lblTitle.BackColor = Color.Transparent;
             lblTitle.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTitle.ForeColor = Color.Yellow;
-            lblTitle.Location = new Point(106, 9);
+            lblTitle.Location = new Point(123, 9);
             lblTitle.Name = "lblTitle";
             lblTitle.Size = new Size(154, 45);
             lblTitle.TabIndex = 0;
             lblTitle.Text = "WORDLE";
+            lblTitle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Gray;
+            label1.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(55, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 50);
+            label1.TabIndex = 1;
+            label1.Text = "  ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Gray;
+            label2.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(115, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 50);
+            label2.TabIndex = 2;
+            label2.Text = "  ";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Gray;
+            label3.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(175, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 50);
+            label3.TabIndex = 3;
+            label3.Text = "  ";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Gray;
+            label4.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(235, 80);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 50);
+            label4.TabIndex = 4;
+            label4.Text = "  ";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.Gray;
+            label5.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(295, 80);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 50);
+            label5.TabIndex = 5;
+            label5.Text = "  ";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.BackColor = Color.Gray;
+            label10.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(295, 145);
+            label10.Name = "label10";
+            label10.Size = new Size(50, 50);
+            label10.TabIndex = 10;
+            label10.Text = "  ";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.BackColor = Color.Gray;
+            label9.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(235, 145);
+            label9.Name = "label9";
+            label9.Size = new Size(50, 50);
+            label9.TabIndex = 9;
+            label9.Text = "  ";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            label8.BackColor = Color.Gray;
+            label8.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(175, 145);
+            label8.Name = "label8";
+            label8.Size = new Size(50, 50);
+            label8.TabIndex = 8;
+            label8.Text = "  ";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.BackColor = Color.Gray;
+            label7.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(115, 145);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 50);
+            label7.TabIndex = 7;
+            label7.Text = "  ";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.Gray;
+            label6.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(55, 145);
+            label6.Name = "label6";
+            label6.Size = new Size(50, 50);
+            label6.TabIndex = 6;
+            label6.Text = "  ";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            label15.BackColor = Color.Gray;
+            label15.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label15.ForeColor = Color.White;
+            label15.Location = new Point(295, 210);
+            label15.Name = "label15";
+            label15.Size = new Size(50, 50);
+            label15.TabIndex = 15;
+            label15.Text = "  ";
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            label14.BackColor = Color.Gray;
+            label14.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label14.ForeColor = Color.White;
+            label14.Location = new Point(235, 210);
+            label14.Name = "label14";
+            label14.Size = new Size(50, 50);
+            label14.TabIndex = 14;
+            label14.Text = "  ";
+            label14.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            label13.BackColor = Color.Gray;
+            label13.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label13.ForeColor = Color.White;
+            label13.Location = new Point(175, 210);
+            label13.Name = "label13";
+            label13.Size = new Size(50, 50);
+            label13.TabIndex = 13;
+            label13.Text = "  ";
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            label12.BackColor = Color.Gray;
+            label12.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label12.ForeColor = Color.White;
+            label12.Location = new Point(115, 210);
+            label12.Name = "label12";
+            label12.Size = new Size(50, 50);
+            label12.TabIndex = 12;
+            label12.Text = "  ";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            label11.BackColor = Color.Gray;
+            label11.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(55, 210);
+            label11.Name = "label11";
+            label11.Size = new Size(50, 50);
+            label11.TabIndex = 11;
+            label11.Text = "  ";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label20
+            // 
+            label20.BackColor = Color.Gray;
+            label20.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label20.ForeColor = Color.White;
+            label20.Location = new Point(295, 275);
+            label20.Name = "label20";
+            label20.Size = new Size(50, 50);
+            label20.TabIndex = 20;
+            label20.Text = "  ";
+            label20.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label19
+            // 
+            label19.BackColor = Color.Gray;
+            label19.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label19.ForeColor = Color.White;
+            label19.Location = new Point(235, 275);
+            label19.Name = "label19";
+            label19.Size = new Size(50, 50);
+            label19.TabIndex = 19;
+            label19.Text = "  ";
+            label19.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label18
+            // 
+            label18.BackColor = Color.Gray;
+            label18.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label18.ForeColor = Color.White;
+            label18.Location = new Point(175, 275);
+            label18.Name = "label18";
+            label18.Size = new Size(50, 50);
+            label18.TabIndex = 18;
+            label18.Text = "  ";
+            label18.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label17
+            // 
+            label17.BackColor = Color.Gray;
+            label17.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label17.ForeColor = Color.White;
+            label17.Location = new Point(115, 275);
+            label17.Name = "label17";
+            label17.Size = new Size(50, 50);
+            label17.TabIndex = 17;
+            label17.Text = "  ";
+            label17.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label16
+            // 
+            label16.BackColor = Color.Gray;
+            label16.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label16.ForeColor = Color.White;
+            label16.Location = new Point(55, 275);
+            label16.Name = "label16";
+            label16.Size = new Size(50, 50);
+            label16.TabIndex = 16;
+            label16.Text = "  ";
+            label16.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label25
+            // 
+            label25.BackColor = Color.Gray;
+            label25.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label25.ForeColor = Color.White;
+            label25.Location = new Point(295, 340);
+            label25.Name = "label25";
+            label25.Size = new Size(50, 50);
+            label25.TabIndex = 25;
+            label25.Text = "  ";
+            label25.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label24
+            // 
+            label24.BackColor = Color.Gray;
+            label24.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label24.ForeColor = Color.White;
+            label24.Location = new Point(235, 340);
+            label24.Name = "label24";
+            label24.Size = new Size(50, 50);
+            label24.TabIndex = 24;
+            label24.Text = "  ";
+            label24.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label23
+            // 
+            label23.BackColor = Color.Gray;
+            label23.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label23.ForeColor = Color.White;
+            label23.Location = new Point(175, 340);
+            label23.Name = "label23";
+            label23.Size = new Size(50, 50);
+            label23.TabIndex = 23;
+            label23.Text = "  ";
+            label23.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label22
+            // 
+            label22.BackColor = Color.Gray;
+            label22.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label22.ForeColor = Color.White;
+            label22.Location = new Point(115, 340);
+            label22.Name = "label22";
+            label22.Size = new Size(50, 50);
+            label22.TabIndex = 22;
+            label22.Text = "  ";
+            label22.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label21
+            // 
+            label21.BackColor = Color.Gray;
+            label21.Font = new Font("Arial", 24F, FontStyle.Bold);
+            label21.ForeColor = Color.White;
+            label21.Location = new Point(55, 340);
+            label21.Name = "label21";
+            label21.Size = new Size(50, 50);
+            label21.TabIndex = 21;
+            label21.Text = "  ";
+            label21.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // keyLabelQ
+            // 
+            keyLabelQ.BackColor = Color.Gray;
+            keyLabelQ.Font = new Font("Arial", 24F, FontStyle.Bold);
+            keyLabelQ.ForeColor = Color.White;
+            keyLabelQ.Location = new Point(12, 450);
+            keyLabelQ.Name = "keyLabelQ";
+            keyLabelQ.Size = new Size(50, 50);
+            keyLabelQ.TabIndex = 26;
+            keyLabelQ.Text = "Q";
+            keyLabelQ.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // keyLabelW
+            // 
+            keyLabelW.BackColor = Color.Gray;
+            keyLabelW.Font = new Font("Arial", 24F, FontStyle.Bold);
+            keyLabelW.ForeColor = Color.White;
+            keyLabelW.Location = new Point(68, 450);
+            keyLabelW.Name = "keyLabelW";
+            keyLabelW.Size = new Size(50, 50);
+            keyLabelW.TabIndex = 27;
+            keyLabelW.Text = "W";
+            keyLabelW.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
-            ClientSize = new Size(384, 461);
+            ClientSize = new Size(589, 670);
+            Controls.Add(keyLabelW);
+            Controls.Add(keyLabelQ);
+            Controls.Add(label25);
+            Controls.Add(label24);
+            Controls.Add(label23);
+            Controls.Add(label22);
+            Controls.Add(label21);
+            Controls.Add(label20);
+            Controls.Add(label19);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(lblTitle);
             Name = "frmMain";
             Text = "Wordle";
             Load += frmMain_Load;
+            Resize += frmMain_Resize;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label lblTitle;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label15;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label20;
+        private Label label19;
+        private Label label18;
+        private Label label17;
+        private Label label16;
+        private Label label25;
+        private Label label24;
+        private Label label23;
+        private Label label22;
+        private Label label21;
+        private Label keyLabelQ;
+        private Label keyLabelW;
     }
 }
--- wordle/Form1.cs
+++ wordle/Form1.cs
@@ -9,30 +9,67 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/* NOTES *
+ * 400x500 original window size jsyk
+ * 
+ * 
+ * 
+*/
 namespace wordle
 {
     public partial class frmMain : Form
     {
 
-        List <Label> letterLabel = new List<Label>();
-        int curCol=0, curRow=0;
+        List<Label> letterLabel = new List<Label>();
+        int curCol = 0, curRow = 0;
         int guesses = 1;
         string word = "";
         HashSet<string> wordList = new HashSet<string>();
-        
+        private Dictionary<Label, Point> labelOffsets = new Dictionary<Label, Point>();
+        private Point titleOffset = new Point();
+
         public frmMain()
         {
             InitializeComponent();
-            generateLabels();
-            loadWords(); 
+
+            //add labels to letterLabel List
+            for(int i=1; i<=25; i++)
+            {
+                Control[] found = this.Controls.Find("label" + i, false); //false - ca sa nu mearga recursiv IN continut
+                letterLabel.Add((Label)found[0]);
+            }
+
+            //
+            int centerX = this.ClientSize.Width / 2;
+            titleOffset.X = lblTitle.Location.X;
+            titleOffset.Y = lblTitle.Location.Y;
+            foreach (Label lbl in letterLabel)
+                labelOffsets[lbl] = new Point(lbl.Location.X - centerX, lbl.Location.Y);
+
+            ///////
+            //generateLabels();
+            loadWords();
             this.KeyPress += frmMain_KeyPress;
             this.KeyPreview = true;
+
         }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        { //vezi cu centratul casutelor
+            int centerX = this.ClientSize.Width / 2;
+            foreach (Label lbl in letterLabel) //(-10) pt SPACING
+                lbl.Location = new Point(labelOffsets[lbl].X + centerX, labelOffsets[lbl].Y);
+
+            int modif = lblTitle.Size.Width / 2;
+            lblTitle.Location = new Point(centerX - modif, lblTitle.Location.Y);
+        } 
 
         private void frmMain_Load(object sender, EventArgs e)
         {
 
         }
+
+
 
         public void loadWords()
         {
@@ -42,27 +79,28 @@ namespace wordle
             try
             {
                 string[] allwords = File.ReadAllLines("wordSelect.txt");
-                foreach(string temp in allwords)
+                foreach (string temp in allwords)
                 {
                     tempWord = temp.Trim(); //remove whitespaces
                     tempWord = tempWord.ToUpperInvariant(); //for diff keyboards
                     wordList.Add(tempWord);
                 }
             }
-            catch(IOException ex)
+            catch (IOException ex) //daca nu convine
             {
                 MessageBox.Show("Problem with the wordle file. wordSelect.txt");
 
             }
             element = rndNum.Next(0, wordList.Count);
             word = wordList.ElementAt(element);
+            
             //word = "GRASS";
-
             //MessageBox.Show("The word is: " + word); //check when running
+            
             try
             {
                 string[] allwords = File.ReadAllLines("validWord.txt");
-                foreach(string temp in allwords)
+                foreach (string temp in allwords)
                 {
                     tempWord = temp.Trim();
                     tempWord = tempWord.ToUpperInvariant();
@@ -74,38 +112,7 @@ namespace wordle
                 MessageBox.Show("Error loading all valid words.");
             }
         }
-    
-        public void generateLabels()
-        {
-            int width = 50;
-            int height = 50;
-            int spacing = 10;
-            int rowWidth = width * 5 + spacing * 4;
-            int x = (this.ClientSize.Width - rowWidth)/2; ///centering the labels; how far from the left
-            int y = 60; /// how far down from the top
 
-            for(int i=0; i<5; i++)
-            {
-                for(int j=0; j<5; j++)
-                {
-                    Label lblLetter = new Label();
-                    lblLetter.Text = "";
-                    lblLetter.Font = new Font("Rockwell", 24, FontStyle.Bold);
-                    lblLetter.Size = new Size(width, height);
-                    lblLetter.TextAlign = ContentAlignment.MiddleCenter;
-                    lblLetter.BorderStyle = BorderStyle.FixedSingle;
-                    lblLetter.BackColor = Color.Gray;
-                    lblLetter.Location = new Point(x + j * (width + spacing), y);
-                    lblLetter.ForeColor = Color.White;
-
-
-                    this.Controls.Add(lblLetter);
-                    letterLabel.Add(lblLetter);
-                }
-                y += 80; ///80 px between each row
-            }
-
-        }
 
         private void frmMain_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -135,33 +142,30 @@ namespace wordle
                 //build the word from list of labels
                 guess = buildWord();
                 //MessageBox.Show("The built word is: " + guess);
-                
-                //check if valid word
+
                 if (checkValid(guess))
                 {
                     guesses++;
-                    updateLabels(guess); //update background color
+                    updateLabels(guess); //background color
 
-                    //check for win
                     if (checkWin(guess))
                     {
                         MessageBox.Show("You win!");
                         Application.Exit();
                         return;
                     }
-                    //check to see if guesses == 6
+                    //check to see if there's a loss
                     else if (guesses == 6)
                     {
                         MessageBox.Show("You ran out of guesses. The word was: " + word);
                         Application.Exit();
                         return;
                     }
+
+                    //NEXT !!
                     curRow++;
                     curCol = curRow * 5;
-
                 }
-
-                //message box for not a valid word
                 else
                 {
                     MessageBox.Show("Not a word");
@@ -172,7 +176,7 @@ namespace wordle
         public string buildWord()
         {
             string builtWord = "";
-            for(int i = curRow*5; i < (curRow+1) * 5; i++)
+            for (int i = curRow * 5; i < (curRow + 1) * 5; i++)
             {
                 builtWord += letterLabel[i].Text;
             }
@@ -182,16 +186,17 @@ namespace wordle
         public void updateLabels(string tempGuess)
         {
             int startIndex = 0;
-            bool[] matchIndex = new bool[5];
-            bool[] matchedInGuess = new bool[5]; //randul
-            bool[] matchedInWord = new bool[5]; //cuv in sine
+            bool[] matchIndex = new bool[5]; //if they're green
+            bool[] matchedInGuess = new bool[5]; //matched inside the guessword
+            bool[] matchedInWord = new bool[5]; //prevent from unnecessary yellows
 
             startIndex = curRow * 5;
 
-            for(int i = 0; i<5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 if (tempGuess[i] == word[i]) //comparing letters
                 {
+                    //letter is good :), pos is good :)
                     letterLabel[startIndex + i].BackColor = Color.Green;
                     matchIndex[i] = true;
                     matchedInGuess[i] = true;
@@ -199,22 +204,28 @@ namespace wordle
                 }
             }
 
-            for(int i = 0; i<5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                if (!matchIndex[i])
+                if (!matchIndex[i]) //daca litera resp nu e verde/daca n am gasit
                 {
-                    for(int j = 0; j<5; j++)
+                    for (int j = 0; j < 5; j++) //ma uit daca e litera in cuvant
                     {
                         if (tempGuess[i] == word[j] && !matchedInWord[j] && !matchedInGuess[i])
                         {
+                            //letter is good :), pos is bad :(
                             letterLabel[startIndex + i].BackColor = Color.Gold;
                             matchedInGuess[i] = true;
                             matchedInWord[j] = true;
                             break;
                         }
                     }
+                    if (!matchedInGuess[i]) //letter bad :(
+                    {
+                        letterLabel[startIndex + i].BackColor = Color.DimGray;
+                    }
                 }
             }
+
         }
 
         public bool checkWin(string tempGuess)
@@ -226,5 +237,39 @@ namespace wordle
         {
             return (wordList.Contains(tempGuess));
         }
+
+        public void generateLabels()
+        {
+            int width = 50;
+            int height = 50;
+            int spacing = 10;
+            int rowWidth = width * 5 + spacing * 4;
+
+            int x = (this.ClientSize.Width - rowWidth) / 2; ///centering the labels; how far from the left
+            int y = 60; /// how far down from the top
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Label lblLetter = new Label();
+                    lblLetter.Text = "";
+                    lblLetter.Font = new Font("Arial", 24, FontStyle.Bold);
+                    lblLetter.Size = new Size(width, height);
+                    lblLetter.TextAlign = ContentAlignment.MiddleCenter;
+                    lblLetter.BorderStyle = BorderStyle.FixedSingle;
+                    lblLetter.BackColor = Color.DarkGray;
+                    lblLetter.Location = new Point(x + j * (width + spacing), y);
+                    lblLetter.ForeColor = Color.White;
+
+
+                    this.Controls.Add(lblLetter);
+                    letterLabel.Add(lblLetter);
+                }
+                y += 80; ///80 px between each row
+            }
+
+        }
+
     }
 }
